43792f876e23a6c67c7397aff189c32bbfdefe52
Abderrahim MECHACHE <abderrahimmechache@MacBook-Pro-de-Abderrahim.local>
2026-03-25 12:56:12 +0100

    Add user profile endpoint

--- api/views.py
+++ api/views.py
@@ -1,7 +1,13 @@
 from rest_framework import viewsets
-from .models import PortfolioItem
-from .serializers import PortfolioItemSerializer
+from .models import PortfolioItem, UserProfile
+from .serializers import PortfolioItemSerializer, UserProfileSerializer
+
 
 class PortfolioItemViewSet(viewsets.ModelViewSet):
     queryset = PortfolioItem.objects.all()
     serializer_class = PortfolioItemSerializer
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
--- backend/urls.py
+++ backend/urls.py
@@ -18,10 +18,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from api.views import PortfolioItemViewSet
+from api.views import PortfolioItemViewSet, UserProfileViewSet
 
 router = DefaultRouter()
 router.register(r'portfolio', PortfolioItemViewSet)
+router.register(r'profiles', UserProfileViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
